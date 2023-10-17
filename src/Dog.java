798c7c1128cba876c7464644ee5a0a057f04b55f
Desmend Jetton <desmendjetton@gmail.com>
2023-10-16 23:58:57 -0500

    scratch pad on class creation

---
+++ src/Dog.java
@@ -0,0 +1,27 @@
+public class Dog {
+    //fields
+    float furLength;
+    String color;
+    String breed;
+    int age;
+    float weight;
+
+
+
+    //methods
+    void bark(){
+        System.out.println("woof!");
+    }
+
+    void sleep(){
+    System.out.println("zzzzz!");
+    }
+
+    void wagTail() {
+        System.out.println("wagging");
+    }
+
+    void run() {
+        System.out.println("zoom!");
+    }
+}
